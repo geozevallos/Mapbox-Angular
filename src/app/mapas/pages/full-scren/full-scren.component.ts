a148f04a5f76bf201641610d0870f9cd1069c0c6
Geojorge <jorgezevallosr@gmail.com>
2021-09-15 20:58:42 -0500

    Creando primer mapa con Mapbox

--- src/app/mapas/pages/full-scren/full-scren.component.ts
+++ src/app/mapas/pages/full-scren/full-scren.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+// Para importar lo de js como angular
+import * as mapboxgl from 'mapbox-gl'
+import { environment } from '../../../../environments/environment';
+
 @Component({
   selector: 'app-full-scren',
   templateUrl: './full-scren.component.html',
   styles: [
+    `
+    #mapa{
+      width: 100%;
+    height: 100%;
+    }`
   ]
 })
 export class FullScrenComponent implements OnInit {
@@ -11,6 +20,12 @@ export class FullScrenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
   }
 
 }
